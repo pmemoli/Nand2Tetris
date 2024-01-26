dd30beb9d4f8c87b02aed4cb3c14178b10ed4dd3
pmemoli <pedromemoli@hotmail.com>
2024-01-26 12:11:47 -0300

    empezado el front del compilador

---
+++ 10/Compiler/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "modules/JackTokenizer.h"
+#include "modules/CompilationEngine.h"
+using namespace std;
+
+int main() {
+    ifstream inputStream();
+
+    return 0;
+}
